634efc4402494fae672f9c1ca2e1bcbf6ec20ed6
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-09-22 22:50:44 +0200

    Add button to get next state of cell matrix

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Cell, CellMatrix, CellState } from "../types/game";
 import { initializeCellMatrix } from "../core/game";
 import CellSquare from "./CellSquare";
+import { getNextCellMatrixState } from "../core/rules";
 
 const GameBoard = ({ rows, columns }: { rows: number, columns: number }) => {
     const [cellMatrix, setCellMatrix] = useState<CellMatrix>(initializeCellMatrix(rows, columns));
@@ -10,10 +11,14 @@ const GameBoard = ({ rows, columns }: { rows: number, columns: number }) => {
         const nextState: CellState = cell.state === 'alive' ? 'dead' : 'alive';
         const { xAxis, yAxis } = cell.coordinates;
 
-        const updatedCellMatrix: CellMatrix = [ ...cellMatrix ];
+        const updatedCellMatrix: CellMatrix = [...cellMatrix];
         updatedCellMatrix[xAxis][yAxis].state = nextState;
 
         setCellMatrix(updatedCellMatrix);
+    };
+
+    const handleGetNextState = () => {
+        setCellMatrix((current) => getNextCellMatrixState(current));
     };
 
     return <>
@@ -30,6 +35,11 @@ const GameBoard = ({ rows, columns }: { rows: number, columns: number }) => {
                 }
             </div>)
         }
+        <div className="py-2 w-72">
+            <button className="px-4 py-2 w-full bg-blue-500 text-white rounded hover:bg-blue-600" onClick={handleGetNextState}>
+                Get next state
+            </button>
+        </div>
     </>;
 };
 
