8ae4940b476fc39a58ac4d68d71626d301fe2de6
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-09-22 22:06:44 +0200

    Add basic game board component

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,3 +1,12 @@
-const GameCellMatrix = () => {
-    
+import { useState } from "react";
+import { CellMatrix } from "../types/game";
+import { initializeCellMatrix } from "../core/game";
+
+const GameBoard = ({rows, columns}: {rows: number, columns: number}) => {
+    const [cellMatrix, setCellMatrix] = useState<CellMatrix>(initializeCellMatrix(rows, columns));
+
+
+    return <div></div>;
 }
+
+export default GameBoard;
