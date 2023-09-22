2fd5b5cf50f62aef87a15acca1bc10aabbf0b534
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-09-22 22:37:48 +0200

    Increase dimensions of board

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GameBoard from "./components/GameBoard";
 function App() {
   return (
     <div className="w-screen h-screen bg-white flex flex-col justify-center items-center p-2">
-      <GameBoard rows={3} columns={3} />
+      <GameBoard rows={15} columns={15} />
     </div >
   );
 }
