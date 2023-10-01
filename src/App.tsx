c206128a1faa31319dee12f8909d3fd56db345ee
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-10-01 20:00:37 +0200

    Make width of gameboard the same as window

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import GameBoard from './components/GameBoard';
 function App() {
   return (
     <div className="w-screen h-screen bg-white flex flex-col justify-center items-center p-2">
-      <GameBoard rows={40} columns={60} />
+      <GameBoard rows={40} columns={64} />
     </div >
   );
 }
--- src/types/game.ts
+++ src/types/game.ts
@@ -2,7 +2,6 @@ export type CellState = 'dead' | 'alive'
 
 export interface Cell {
     state: CellState
-    //coordinates: Coordinates
 }
 
 export interface Coordinates {
