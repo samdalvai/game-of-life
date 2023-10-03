c02a912671f6caf681e11ecfa30deb7636e05b1b
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-10-03 08:15:43 +0200

    Add start game button for main menu

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import GameLogo from "./GameLogo";
+import { useState } from 'react';
+import GameLogo from './GameLogo';
+import Button from './Button';
 
 const MainMenu = () => {
     const [startGame, setStartGame] = useState<boolean>(false);
@@ -7,6 +8,9 @@ const MainMenu = () => {
     return <div className="w-full h-full md:w-96 md:h-96 rounded-md flex flex-col justify-center items-center bg-white">
         <h1 className="font-bold text-xl py-1">Game of Life</h1>
         <GameLogo />
+        <div className="py-2">
+            <Button text="Start game" color="blue" onClick={() => setStartGame(true)} />
+        </div>
     </div>;
 };
 
