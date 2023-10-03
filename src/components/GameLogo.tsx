4d761f13112282c3c2e488fb1796e4190fd0ddc0
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-10-03 08:05:57 +0200

    Add game logo

---
+++ src/components/GameLogo.tsx
@@ -0,0 +1,25 @@
+const GameLogo = () => {
+    const Square = ({ color }: { color: 'black' | 'white' }) => {
+        return <div className={`w-10 h-10 ${color === 'black' ? 'bg-black' : 'bg-white'} flex-shrink-0 border-solid border border-slate-500 rounded-sm`} />;
+    };
+
+    return <div>
+        <div className="flex justify-center">
+            <Square color="white" />
+            <Square color="black" />
+            <Square color="white" />
+        </div>
+        <div className="flex justify-center">
+            <Square color="white" />
+            <Square color="white" />
+            <Square color="black" />
+        </div>
+        <div className="flex justify-center">
+            <Square color="black" />
+            <Square color="black" />
+            <Square color="black" />
+        </div>
+    </div>;
+};
+
+export default GameLogo;
---
+++ src/components/MainMenu.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const MainMenu = () => {
+    const [startGame, setStartGame] = useState<boolean>(false);
+
+    return <div>
+        Hello
+    </div>;
+};
+
+export default MainMenu;
