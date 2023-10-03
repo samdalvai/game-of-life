45c7ed39cf3b4b47484b89637f0b01460715b487
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-10-03 08:10:47 +0200

    Add main menu with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import GameBoard from './components/GameBoard';
-import GameLogo from './components/GameLogo';
 import MainMenu from './components/MainMenu';
 
 function App() {
   return (
     <div className="w-screen h-screen bg-slate-300 flex flex-col justify-center items-center p-2">
-      <GameLogo />
+      <MainMenu />
     </div >
   );
 }
--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import GameLogo from "./GameLogo";
 
 const MainMenu = () => {
     const [startGame, setStartGame] = useState<boolean>(false);
 
-    return <div>
-        Hello
+    return <div className="w-full h-full md:w-96 md:h-96 rounded-md flex flex-col justify-center items-center bg-white">
+        <h1 className="font-bold text-xl py-1">Game of Life</h1>
+        <GameLogo />
     </div>;
 };
 
