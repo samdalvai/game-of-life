5ad556491b9b697a297167a834a8f64b01f536bb
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-05 18:00:04 +0100

    Fix tailwind config linter errors

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -3,8 +3,7 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
-
