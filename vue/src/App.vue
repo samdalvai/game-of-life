1f8e2de460da3106021800e1ca8218fde02c5fac
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-08 22:20:26 +0100

    Add cell object as prop to CellSquare

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import CellSquare from './components/cells/CellSquare.vue';
+import { Cell } from 'game-of-life-core';
 
+const cell: Cell = { state: 'dead' };
 </script>
 
 <template>
   <div className="w-screen h-screen bg-slate-200 flex flex-col justify-center items-center p-2">
-    <CellSquare :cell="{ state: 'dead' }" />
+    <CellSquare :cell="cell" />
   </div>
 </template>
