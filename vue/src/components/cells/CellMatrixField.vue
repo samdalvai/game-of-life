ac68d2df095d9f54123ee15a302c1b6fa11ca021
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-08 23:14:38 +0100

    Add cell matrix component

---
+++ vue/src/components/cells/CellMatrixField.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { defineProps, defineEmits, computed } from 'vue';
+import CellSquare from './components/cells/CellSquare.vue';
+import { CellMatrix, Cell } from 'game-of-life-core';
+
+const props = defineProps<{ cellMatrix: CellMatrix }>();
+
+const handleClick = () => {
+  console.log("clicked cell")
+};
+
+</script>
+
+<template>
+  <div class="border-2 border-black">
+    <li v-for="(cell, index) in props.cellMatrix[0]" :key="index">
+        <CellSquare :cell="cell" @click="handleClick"/>
+    </li>
+  </div>
+</template>
