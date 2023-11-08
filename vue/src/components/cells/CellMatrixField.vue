9f39c669315d22685d4f1720523f745526f8ba15
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-08 23:29:20 +0100

    Render two dimensional cell matrix

--- vue/src/components/cells/CellMatrixField.vue
+++ vue/src/components/cells/CellMatrixField.vue
@@ -12,9 +12,9 @@ const handleClick = () => {
 </script>
 
 <template>
-  <div class="border-2 border-black">
-    <div v-for="(cell, index) in props.cellMatrix[0]" :key="index">
-        <CellSquare :cell="cell" @click="handleClick"/>
+    <div class="border-2 border-black">
+        <div class="flex justify-center" v-for="(cellRow, xAxis) in props.cellMatrix" :key="'row-' + xAxis">
+            <CellSquare v-for="(cell, yAxis) in cellRow" :key="'cell-' + yAxis" :cell="cell" @click="handleClick" />
+        </div>
     </div>
-  </div>
 </template>
