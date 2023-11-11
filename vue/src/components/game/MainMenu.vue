2997986f6c0ef6ff70f39001f6d8184e05630781
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-11 22:21:33 +0100

    Add initialization of game board in main menu

--- vue/src/components/game/MainMenu.vue
+++ vue/src/components/game/MainMenu.vue
@@ -1,19 +1,41 @@
 <script lang="ts" setup>
-import { ref, defineProps, defineEmits } from 'vue';
+import { ref } from 'vue';
 import GameBoard from './GameBoard.vue';
 import Button from '../core/Button.vue';
+import { GameBoardSize } from '@/types/components';
 
-const startGame = ref(false);
+const startGame = ref<boolean>(false);
+const gameBoardSize = ref<GameBoardSize>(getDefaultGameBoardSize());
+const infiniteGameBoard = ref<boolean>(false);
+
+function getDefaultGameBoardSize (): GameBoardSize {
+  if (window.innerWidth <= 640) {
+    return { rows: 40, columns: 25 };
+  }
+
+  if (window.innerWidth <= 768) {
+    return { rows: 40, columns: 50 };
+  }
+
+  return { rows: 40, columns: 70 };
+}
+
+const handleStartGame = () => {
+  if (gameBoardSize.value.rows > 0 && gameBoardSize.value.columns > 0) {
+    startGame.value = true;
+  }
+};
 
 </script>
 
 <template>
-<GameBoard v-if="startGame" :rows="15" :columns="28" :infiniteGameBoard="true"/>
-<div v-else class="w-full h-2/3 lg:w-1/2 rounded-md flex flex-col justify-center items-center bg-white shadow-sm border border-black">
+    <GameBoard v-if="startGame" :rows="gameBoardSize.rows" :columns="gameBoardSize.columns" :infiniteGameBoard="infiniteGameBoard" @back="() => startGame = false" />
+    <div v-else
+        class="w-full h-2/3 lg:w-1/2 rounded-md flex flex-col justify-center items-center bg-white shadow-sm border border-black">
         <h1 class="font-bold text-2xl py-3 text-blue-600">Game of Life</h1>
         <div class="pt-4 pb-2">
             <div class="px-2">
-                <Button :text="'Start game'" :color="'blue'" />
+                <Button :text="'Start game'" :color="'blue'" @click="handleStartGame" />
             </div>
             <div class="flex py-2 px-2">
                 <div class="pe-1 w-1/3">
