0555caf7ac814b589426314bdc8e575a5f9d528a
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-11 23:01:56 +0100

    Remove unused InputType tyope definition

--- react/src/types/components.ts
+++ react/src/types/components.ts
@@ -4,8 +4,6 @@ export type ChildrenProps = ReactNode;
 
 export type ButtonColor = 'blue' | 'gray' | 'green' | 'red' | 'indigo' | 'orange' | 'yellow'
 
-export type InputType = 'text' | 'number' | 'range'
-
 export interface GameBoardSize {
     rows: number,
     columns: number
--- vue/src/types/components.ts
+++ vue/src/types/components.ts
@@ -1,7 +1,5 @@
 export type ButtonColor = 'blue' | 'gray' | 'green' | 'red' | 'indigo' | 'orange' | 'yellow'
 
-export type InputType = 'text' | 'number' | 'range'
-
 export interface GameBoardSize {
     rows: number,
     columns: number
