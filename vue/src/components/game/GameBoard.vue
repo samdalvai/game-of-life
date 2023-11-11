cc9ca3fbf050af3602703fa0e1c72a8f6d413c77
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-11 21:31:28 +0100

    Add basic game board menu with three buttons

--- vue/src/components/game/GameBoard.vue
+++ vue/src/components/game/GameBoard.vue
@@ -2,9 +2,7 @@
 import { ref, defineProps, defineEmits } from 'vue';
 import { CellMatrix, initializeCellMatrix, getNextCellMatrixState, Coordinates, CellState } from 'game-of-life-core';
 import CellMatrixField from '../cells/CellMatrixField.vue';
-import Button from '../core/Button.vue';
-import NextIcon from '../icons/NextIcon.vue';
-import PlayIcon from '../icons/PlayIcon.vue';
+import GameBoardMenu from './GameBoardMenu.vue';
 import TimedCounter from '../core/TimedCounter.vue';
 
 const props = defineProps<{ rows: number, columns: number, infiniteGameBoard: boolean }>();
@@ -33,23 +31,23 @@ const handleRunGame = () => {
   gameRunning.value = !gameRunning.value;
 };
 
+const handleResetState = () => {
+  cellMatrix.value = initializeCellMatrix(props.rows, props.columns);
+  gameRunning.value = false;
+};
+
 const handleGetNextState = () => {
   cellMatrix.value = getNextCellMatrixState(cellMatrix.value, props.infiniteGameBoard);
 };
 </script>
 
 <template>
-    <div className="w-full h-screen py-2 xl:w-2/3 flex flex-col justify-center">
-        <div className="flex justify-center">
-            <h1 className="font-bold text-2xl py-3 text-blue-600">Game of Life</h1>
-        </div>
-        <CellMatrixField :cellMatrix="cellMatrix" @cellClick="handleCellClick"/>
-        <Button :text="'Run'" :color="'green'" @click="handleRunGame">
-          <PlayIcon />
-        </Button>
-        <Button :text="'Next'" :color="'blue'" @click="handleGetNextState">
-          <NextIcon />
-        </Button>
+  <div className="w-full h-screen py-2 xl:w-2/3 flex flex-col justify-center">
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl py-3 text-blue-600">Game of Life</h1>
     </div>
-    <TimedCounter v-if="gameRunning" :interval="100" @count="handleGetNextState" />
+    <CellMatrixField :cellMatrix="cellMatrix" @cellClick="handleCellClick" />
+    <GameBoardMenu @run="handleRunGame" @reset="handleResetState" @next="handleGetNextState" />
+  </div>
+  <TimedCounter v-if="gameRunning" :interval="100" @count="handleGetNextState" />
 </template>
---
+++ vue/src/components/game/GameBoardMenu.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { ref, defineProps, defineEmits } from 'vue';
+import Button from '../core/Button.vue';
+import PlayIcon from '../icons/PlayIcon.vue';
+import DeleteIcon from '../icons/DeleteIcon.vue';
+import NextIcon from '../icons/NextIcon.vue';
+
+const emit = defineEmits(['run', 'reset', 'next']);
+
+const emitRun = () => {
+  emit('run', null);
+};
+
+const emitReset = () => {
+  emit('reset', null);
+};
+
+const emitNext = () => {
+  emit('next', null);
+};
+
+</script>
+
+<template>
+    <div className="pt-3 w-full flex flex-col md:flex-row">
+        <div className="w-full md:w-3/5 flex md:pe-1">
+            <div className="pe-1 w-1/3">
+                <Button :text="'Run'" :color="'green'" @click="emitRun">
+                    <PlayIcon />
+                </Button>
+            </div>
+            <div className="px-1 w-1/3">
+                <Button :text="'Reset'" :color="'gray'" @click="emitReset">
+                    <DeleteIcon />
+                </Button>
+            </div>
+            <div className="ps-1 w-1/3">
+                <Button :text="'Next'" :color="'blue'" @click="emitNext">
+                    <NextIcon />
+                </Button>
+            </div>
+        </div>
+        <div className="w-full md:w-2/5 flex pt-2 md:ps-1 md:pt-0">
+            <div className="pe-1 w-1/2">
+
+            </div>
+            <div className="ps-1 w-1/2">
+
+            </div>
+        </div>
+    </div>
+</template>
