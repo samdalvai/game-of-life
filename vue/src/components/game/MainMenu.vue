5afbfbee295cf1f15ac992d9ce9d6d49a9533066
Samuel Dalvai <samuel.dalvai@ewo.com>
2023-11-14 21:47:56 +0100

    Add slider input component

--- vue/src/components/game/MainMenu.vue
+++ vue/src/components/game/MainMenu.vue
@@ -5,10 +5,12 @@ import GameLogo from '../core/GameLogo.vue';
 import Button from '../core/Button.vue';
 import NumberInput from '../core/NumberInput.vue';
 import CheckBoxInput from '../core/CheckBoxInput.vue';
+import SliderInput from '../core/SliderInput.vue';
 import { GameBoardSize } from '@/types/components';
 
 const startGame = ref<boolean>(false);
 const infiniteGameBoard = ref<boolean>(false);
+const updateSpeed = ref<number>(100);
 
 const getDefaultGameBoardSize = (): GameBoardSize => {
   if (window.innerWidth <= 640) {
@@ -35,32 +37,41 @@ const handleGameBoardSizeChange = (event: any) => {
   gameBoardSize.value = { ...gameBoardSize.value, [event.target.name]: newValue };
 };
 
+const updateSpeedChange = (event: any) => {
+  updateSpeed.value = parseInt(event.target.value);
+};
+
 </script>
 
 <template>
-    <GameBoard v-if="startGame" :rows="gameBoardSize.rows" :columns="gameBoardSize.columns"
-        :infiniteGameBoard="infiniteGameBoard" @back="() => startGame = false" />
-    <div v-else
-        class="w-full h-2/3 lg:w-1/2 rounded-md flex flex-col justify-center items-center bg-white shadow-sm border border-black">
-        <h1 class="font-bold text-2xl py-3 text-blue-600">Game of Life</h1>
-        <GameLogo />
-        <div class="pt-4 pb-2">
-            <div class="px-2">
-                <Button :text="'Start game'" :color="'blue'" @click="handleStartGame" />
-            </div>
-            <div class="flex py-2 px-2">
-                <div class="pe-1 w-1/3">
-                    <NumberInput :label="'Rows'" :name="'rows'" :value="gameBoardSize.rows"
-                        @change="handleGameBoardSizeChange" />
-                </div>
-                <div class="px-1 w-1/3">
-                    <NumberInput :label="'Columns'" :name="'columns'" :value="gameBoardSize.columns"
-                        @change="handleGameBoardSizeChange" />
-                </div>
-                <div class="ps-1 w-1/3">
-                    <CheckBoxInput :label="'Infinite board'" :name="'infinite-board'" :checked="infiniteGameBoard"
-                        @change="() => infiniteGameBoard = !infiniteGameBoard" />
-                </div>
-            </div>
+  <GameBoard v-if="startGame" :rows="gameBoardSize.rows" :columns="gameBoardSize.columns"
+    :infiniteGameBoard="infiniteGameBoard" @back="() => startGame = false" />
+  <div v-else
+    class="w-full h-2/3 lg:w-1/2 rounded-md flex flex-col justify-center items-center bg-white shadow-sm border border-black">
+    <h1 class="font-bold text-2xl py-3 text-blue-600">Game of Life</h1>
+    <GameLogo />
+    <div class="pt-4 pb-2">
+      <div class="px-2">
+        <Button :text="'Start game'" :color="'blue'" @click="handleStartGame" />
+      </div>
+      <div class="flex py-2 px-2">
+        <div class="pe-1 w-1/2">
+          <NumberInput :label="'Rows'" :name="'rows'" :value="gameBoardSize.rows" @change="handleGameBoardSizeChange" />
         </div>
-</div></template>
+        <div class="px-1 w-1/2">
+          <NumberInput :label="'Columns'" :name="'columns'" :value="gameBoardSize.columns"
+            @change="handleGameBoardSizeChange" />
+        </div>
+      </div>
+      <div class="flex py-2 px-2">
+        <div class="pe-1 w-1/2">
+          <SliderInput :label="'Update speed'" :name="'update-speed'" :value="updateSpeed" @change="updateSpeedChange" />
+        </div>
+        <div class="ps-1 w-1/3">
+          <CheckBoxInput :label="'Infinite board'" :name="'infinite-board'" :checked="infiniteGameBoard"
+            @change="() => infiniteGameBoard = !infiniteGameBoard" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
